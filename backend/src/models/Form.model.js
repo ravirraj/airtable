c9f79b02dd2eb46e6ea4959d65ab7bac76178de4
yami <ravirrajpatil@gmail.com>
2025-12-06 16:56:05 +0530

    feat: add Form model with schemas for conditions, questions, and forms

---
+++ backend/src/models/Form.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const conditionSchema = new mongoose.Schema(
+  {
+    quetionKey: String,
+    conditionType: {
+      type: String,
+      enum: ["equals", "not equals", "contains"],
+    },
+    value: mongoose.Schema.Types.Mixed,
+  },
+  { _id: false }
+);
+
+const conditionalRulesSchema = new mongoose.Schema(
+  {
+    logic: {
+      type: String,
+      enum: ["AND", "OR"],
+      default: "AND",
+    },
+    conditions: [conditionSchema],
+  },
+  { _id: false }
+);
+
+const questionSchema = new Schema(
+  {
+    questionKey: { type: String, required: true },
+    airtableFieldId: { type: String, required: true },
+    label: String,
+    type: String,
+    required: { type: Boolean, default: false },
+    options: [String],
+    conditionalRules: { type: conditionalRulesSchema, default: null },
+  },
+  { _id: false }
+);
+
+const formSchema = new Schema(
+  {
+    owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: String,
+    airtableBaseId: String,
+    airtableTableId: String,
+    questions: [questionSchema],
+  },
+  { timestamps: true }
+);
+
+
+export const Form = mongoose.model("Form", formSchema);
